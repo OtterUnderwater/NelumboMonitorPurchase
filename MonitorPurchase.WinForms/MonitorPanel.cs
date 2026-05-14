127232d4636bd6b2af356f5ed6da657e39b35a15
OtterUnderwater <teo2005@bk.ru>
2026-05-14 15:41:16 +0300

    init

---
+++ MonitorPurchase.WinForms/MonitorPanel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections;
+using System.Windows.Forms.Integration;
+
+namespace MonitorPurchase.WinForms
+{
+    public partial class MonitorPanel : UserControl
+    {
+        private ElementHost _elementHost;
+        private MonitorControl _wpfControl;
+
+        public MonitorPanel(Hashtable parameters)
+        {
+            try
+            {
+                InitializeComponent();
+                _elementHost = new ElementHost
+                {
+                    Dock = DockStyle.Fill,
+                    Name = "wpfHost"
+                };
+                _wpfControl = new MonitorControl(parameters);
+                _elementHost.Child = _wpfControl;
+                Controls.Add(_elementHost);
+                ConfigurePanel();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Ошибка при загрузке диаграммы:\n\n{ex.Message}\n\n",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                throw;
+            }
+        }
+        private void ConfigurePanel()
+        {
+            this.Dock = DockStyle.Fill;
+            this.BackColor = Color.White;
+            this.AutoScaleMode = AutoScaleMode.None;
+        }
+    }
+}
---
+++ TestDllWindows/Program.cs
@@ -0,0 +1,39 @@
+﻿using MonitorPurchase.WinForms;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TestDllWindows
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Hashtable parameters = new Hashtable();
+            parameters["ConnectionString"] = "server=MB-FW5T1RUAHO2Z;database=_box001;uid=se;pwd=wd;";
+            parameters["RegID"] = 174967;
+            parameters["OrderID"] = 27;
+
+            // Создаем форму и добавляем на нее панель
+            Form hostForm = new Form();
+            hostForm.Text = "Производственный план";
+            hostForm.WindowState = FormWindowState.Maximized;
+            hostForm.StartPosition = FormStartPosition.CenterScreen;
+
+            MonitorPanel panel = new MonitorPanel(parameters);
+            panel.Dock = DockStyle.Fill;
+            hostForm.Controls.Add(panel);
+
+            Application.Run(hostForm);
+        }
+    }
+}
